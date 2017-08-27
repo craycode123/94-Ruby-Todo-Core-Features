e127d5dc266a00ba55a4ba89cae914f22f4c2623
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:10:16 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,24 +1,11 @@
-<div><p>Ruby’s <code class="inline">String</code> class has over <a href="http://www.ruby-doc.org/core-2.1.3/String.html">100 public methods</a>&nbsp;that give Ruby programmers a remarkable power to process, manipulate, and transform textual data. About a dozen of those <code class="inline">String</code> methods use <strong>reg</strong>ular <strong>ex</strong>pressions (regex) in order to allow for high-powered string matching.</p>
-<p>An example of low-powered string matching is when you use your browser’s search (Ctrl-F on a Ubuntu/Linux) for a specific word, say, your own name. What if you wanted to search your browser for anything that matches the pattern for a social security number or a credit card number? For that, you’d use regular expressions.</p>
-<h4>Objectives</h4>
+<div><p>In this challenge, we will be writing methods and appropriate regular expressions to find, hide and format social security numbers. </p>
+<h3>Objectives</h3>
 <ol>
-<li><p>Do <a href="https://regexone.com/">this tutorial</a> to get familiar with pattern matching using regex.</p>
+<li><p>Use the driver code in the gist to guide you to a working solution.</p>
 </li>
-<li><p>Learn how to use <a href="http://rubular.com">http://rubular.com</a>&nbsp;to help you code your regex. </p>
+<li><p>Add additional driver code where you want to feel more comfortable with your implementation. For instance, what happens when you pass a string with only a single SSN into the <code class="inline">hide_all_ssns</code> or <code class="inline">get_all_ssns</code> methods?</p>
 </li>
-<li><p>Figure out how <code class="inline">gsub</code> and the match operator <code class="inline">=~</code> are used in <code class="inline">regex_example.rb</code>. Remember to check the docs to see what <code class="inline">gsub</code> and <code class="inline">=~</code> will return.</p>
-</li>
-<li><p>Complete the exercises given in <code class="inline">regex_exercise.rb</code>. The questions are listed below as well:</p>
-<p>a. Write a method named <code class="inline">"hide_serial(string)"</code> that accepts a string as an argument that uses the regex for <code class="inline">"123456-12-1234"</code> and substitute the first two sections with <code class="inline">"X"</code> to get <code class="inline">"XXXXXX-XX-1234"</code>.</p>
-<p>b. Write a method named <code class="inline">"hide_digits(string)"</code> that accepts a string as an argument that substitutes the digits in the sentence <code class="inline">"You have 100 dollars"</code> with <code class="inline">"-"</code> to get <code class="inline">"You have --- dollars"</code>.</p>
-<p>c. Write a method named <code class="inline">"hide_last_four(string)"</code> that accepts a string as an argument that substitute the last 4 digits with <code class="inline">"*"</code> in this string: <code class="inline">"12-34-5678, 90-80-7012, 45-65-1234"</code> to get <code class="inline">"12-34-****, 90-80-****, 45-65-****"</code>.</p>
-</li>
-</ol>
-<h4>Further reading:</h4>
-<ol>
-<li><p><a href="http://www.ruby-doc.org/docs/ProgrammingRuby/html/language.html#UJ">The Pickaxe Guide</a>&nbsp;</p>
-</li>
-<li><p><a href="http://www.bluebox.net/about/blog/2013/02/using-regular-expressions-in-ruby-part-1-of-3/">ReGex Overview</a> </p>
+<li><p>Did adding driver code help you solve the challenge more effectively? How? (Answer in the gist as a comment)</p>
 </li>
 </ol>
 </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,22 +1,74 @@
 require 'rspec'
-require_relative 'regex_exercise'
-describe 'regexp' do
-  describe 'hide_serial' do
-    it 'should return value XXXXXX-XX-1234' do
-      string = '123456-12-1234'
-      expect(hide_serial(string)).to eq('XXXXXX-XX-1234')
+require_relative "regular_expressions"
+
+describe "regular_expressions.rb" do
+  let(:ssn){ "234-60-1422" }
+  let(:arg_with_ssn){ "please don't share this: #{ssn}" }
+  let(:arg_without_ssn){ "please confirm your identity: XXX-XX-1422" }
+  let(:string_with_ssns){ "234-60-1422, 350-80-0744, 013-60-8762" }
+  let(:ssn_regex){ /(\d{3})\D?(\d{2})\D?(\d{4})/ }
+
+  describe "#has_ssn?" do
+    it "returns true if it has what looks like a SSN" do
+      result = has_ssn?(arg_with_ssn)
+      expect(result).to eq true
+    end
+
+    it "returns false if it doesn't have a SSN" do
+      result = has_ssn?(arg_without_ssn)
+      expect(result).to eq false
     end
   end
-  describe 'hide_digits' do
-    it 'should replace all digits with - symbol' do
-      string = 'You have 100 dollars'
-      expect(hide_digits(string)).to eq('You have --- dollars')
+
+  describe "#grab_ssn" do
+    it "returns an SSN if the string has an SSN" do
+      result = grab_ssn(arg_with_ssn)
+      expect(result).to eq ssn
+    end
+
+    it "returns nil if it doesn't have a SSN" do
+      result = grab_ssn(arg_without_ssn)
+      expect(result).to eq nil
     end
   end
-  describe 'hide_last_four' do
-    it 'should replace last 4 digits of each group to ****' do
-      string = '12-34-5678, 90-80-7012, 45-65-1234'
-      expect(hide_last_four(string)).to eq("12-34-****, 90-80-****, 45-65-****")
+
+  describe "#grab_all_ssns" do
+    it "returns all SSNs if the string has any SSNs" do
+      result = grab_all_ssns(string_with_ssns)
+      expected_result = string_with_ssns.split(", ")
+      expect(result).to eq expected_result
+    end
+
+    it "returns an empty Array if it doesn't have any SSNs" do
+      result = grab_all_ssns(arg_without_ssn)
+      expect(result).to eq []
+    end
+  end
+
+  describe "#hide_all_ssns" do
+    it "obfuscates any SSNs in the string" do
+      result = hide_all_ssns(string_with_ssns)
+      expected_result = string_with_ssns.gsub(ssn_regex, 'XXX-XX-\3')
+      expect(result).to eq expected_result
+    end
+
+    it "does not alter a string without SSNs in it" do
+      result = hide_all_ssns(arg_without_ssn)
+      expect(result).to eq arg_without_ssn
+    end
+  end
+
+  describe "#format_ssns" do
+    let(:weird_ssn_string){ "234601422, 350.80.0744, 013-60-8762" }
+
+    it "finds and reformat any SSNs in the string" do
+      result = format_ssns(weird_ssn_string)
+      expect(result).to eq string_with_ssns
+    end
+
+    it "does not alter a string without SSNs in it" do
+      result = format_ssns(arg_without_ssn)
+      expect(result).to eq arg_without_ssn
     end
   end
 end
