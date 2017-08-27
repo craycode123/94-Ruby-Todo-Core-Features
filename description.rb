aaffedf9d867e593a9c5425bc9a932246e31a775
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:22:44 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,33 +1,28 @@
-<div><p>Children who are learning arithmetic sometimes play a number-guessing game:</p>
-<ul>
-<li>“I’m thinking of a number between 1 and 100. Can you guess it?”
-</li>
-<li><em>“Is the number less than 50?”</em>
-</li>
-<li>“Yes.”
-</li>
-<li><em>“Is the number less than 25?”</em>
-</li>
-<li>“No.”
-</li>
-</ul>
-<p>And so on, halving the interval at each step until only one number is left.</p>
-<p>This technique is known colloquially as the binary chop. It is a well-known technique for searching through a collection. Let’s build it in Ruby.</p>
-<p>Beware that this exercise is harder than it looks. Jon Bentley, in his book Programming Pearls, reports that 90% of professional programmers cannot write a proper implementation of binary search in two hours, and Donald Knuth, in the second volume of his book The Art of Computer Programming, reports that though the first binary search was published in 1946, the first bug-free binary search wasn’t published until 1962.</p>
-<p>If you want to read more, check out the <a href="http://en.wikipedia.org/wiki/Binary_search_algorithm">Wikipedia article</a>.</p>
-<p><cite>Source: <a href="http://pine.fm/LearnToProgram/">Chris Pine, Learn To Program</a>&nbsp;|&nbsp;<a href="http://programmingpraxis.com/2009/03/23/binary-search/">Programming Praxis</a></cite></p>
+<div><p>We’re going to write a Ruby method <code class="inline">factorial</code> to compute the <a href="http://en.wikipedia.org/wiki/Factorial">factorial</a>&nbsp;of an integer. Here’s an example of how a factorial looks like:</p>
+<pre><code class="ruby hljs"><span class="hljs-number">1</span>! = <span class="hljs-number">1</span>
+<span class="hljs-number">2</span>! = <span class="hljs-number">2</span> x <span class="hljs-number">1</span>
+<span class="hljs-number">3</span>! = <span class="hljs-number">3</span> x <span class="hljs-number">2</span> x <span class="hljs-number">1</span>
+<span class="hljs-number">4</span>! = <span class="hljs-number">4</span> x <span class="hljs-number">3</span> x <span class="hljs-number">2</span> x <span class="hljs-number">1</span></code></pre>
+<p>If you prefer a more “mathsy” definition, here’s one: the <strong>factorial</strong> of a non-negative integer n,&nbsp;denoted n!, is the product of all positive integers less than n.</p>
+<p>The formula for a factorial is given by: n! = n x (n-1) x (n-2) x … x 2 x 1.</p>
+<p>The factorial function wasn’t pulled out of a mathematician’s hat - it has a very concrete meaning. For example, if you have 3 balls of different colours (red, green, blue), there are 3! = 6 ways to order those ball. See the picture below and check that there are really 6 ways to order the balls!</p>
+<img style="display: block; margin-left: auto; margin-right: auto; width: 20%;" class="img-responsive" src="https://s3-ap-southeast-1.amazonaws.com/next-academy-learning-portal/static_assets/images/(public)+Permutations_RGB.svg.png"><p></p><p>In general, if you have a set of <strong>n&nbsp;things</strong> then there are <strong>n!&nbsp;ways</strong> to order those things.</p>
+<p>By convention we define</p>
+<img src="https://s.wordpress.com/latex.php?latex=%5Cbegin%7Baligned%7D%0A%20%20%20%20%20%200!%20%3D%201%0A%20%20%20%20%5Cend%7Baligned%7D&amp;bg=ffffff&amp;fg=000000&amp;s=0" alt="zero_factorial"><p>so that we can formally define the factorial as</p>
+<img src="https://s.wordpress.com/latex.php?latex=n!%20%3D%0A%20%20%20%20%20%20%5Cbegin%7Bcases%7D%0A%20%20%20%20%20%20%20%201%20%26%20%5Cmbox%7Bif%20%7D%20n%20%3D%200%20%5C%5C%0A%20%20%20%20%20%20%20%20n%20%5Ccdot%20(n-1)!%20%26%20%5Cmbox%7Botherwise%7D%0A%20%20%20%20%20%20%5Cend%7Bcases%7D&amp;bg=ffffff&amp;fg=000000&amp;s=0" alt="factorial"><p>Basically, what’s happening in the above formula is this:</p>
+<p>When <strong>n = 0</strong>, 0! = 1 (that’s the first iteration)</p>
+<p>When <strong>n = 1</strong>, n! = 1! = &nbsp;1.</p>
+<p>When <strong>n = 2</strong>, n! = 2! = 2 x (2 - 1)! = 2 x 1 = 2.</p>
+<p>And so on so forth.&nbsp;</p>
+<p>This definition is called <strong>recursive</strong> because we define the factorial of <strong>n</strong>&nbsp;in terms of the factorial of <strong>n-1</strong>. We stop at the <strong>base case</strong>, where n = 0. This is called a <a href="https://en.wikipedia.org/wiki/Recursion_termination">recursive termination</a>&nbsp;whereby a condition is met (here the condition is&nbsp;<img src="http://s.wordpress.com/latex.php?latex=n%2520%253D%25200&amp;bg=ffffff&amp;fg=000000&amp;s=0" alt="">) such that recursive algorithm can stop calling itself and start returning values.</p>
 <h3>Tasks</h3>
-<h4>Write the algorithm</h4>
-<p>Your first task is to write a method that takes a target number and a <strong>sorted</strong> array of numbers in non-decreasing order and returns either the position of the number in the array, or <code class="inline">-1</code> to indicate the target number is not in the array. For instance, <code class="inline">binary_search(32, [13, 19, 24, 29, 32, 37, 43])</code> should return <code class="inline">4</code>, since <code class="inline">32</code> is the fourth element of the array (counting from zero). Start with pseudocode! What are the steps? What’s the end case? Spell it out before jumping into Ruby. You can’t use <code class="inline">Array#index</code>. Sorry. The point is to build a binary search from scratch, not use Ruby’s built in search methods. Also, don’t use <code class="inline">Array#.include?</code>.</p>
-<p>HINT: Here’s an implementation of binary search in <a href="http://codereview.stackexchange.com/questions/5363/efficient-binary-search">JavaScript</a>. You can start creating your pseudocode with this as a model if you like.</p>
-<h4>Ensuring accuracy</h4>
-<p>Write a suitable test program that shows the accuracy of your binary search method. Test your method against a variety of inputs. Does it always return the correct answer?</p>
-<p>A basic testing program could look like this (yours should test more examples):</p>
-<pre><code class="ruby hljs">test_array = (<span class="hljs-number">100</span>..<span class="hljs-number">200</span>).to_a
-puts binary_search(<span class="hljs-number">135</span>, test_array) == <span class="hljs-number">35</span>
-<span class="hljs-comment"># =&gt; true</span>
-
-test_array = [<span class="hljs-number">13</span>, <span class="hljs-number">19</span>, <span class="hljs-number">24</span>, <span class="hljs-number">29</span>, <span class="hljs-number">32</span>, <span class="hljs-number">37</span>, <span class="hljs-number">43</span>]
-puts binary_search(<span class="hljs-number">35</span>, test_array) == -<span class="hljs-number">1</span>
-<span class="hljs-comment"># =&gt; true</span></code></pre>
+<h4>1. Factorial, Iteratively</h4>
+<p>Implement an iterative version of the factorial function called <code class="inline">factorial_iterative</code>. It should accept any non-negative integer as input. Don’t worry about handling bad input.</p>
+<h4>2. Factorial, Recursively</h4>
+<p>The focus this time around will be on understanding <a href="http://en.wikipedia.org/wiki/Recursion_%2528computer_science%2529">recursion</a>. This is your first run-in with <strong>recursion</strong>. It will seem strange at first, but you’ll quickly get used to it. Eventually, you’ll develop a kind of sixth sense and start saying things like, “This feels like a recursive problem.”</p>
+<p>The most important thing to understand is that every recursive problem has some number of <strong>base cases</strong> , the condition that acts like the “kick” from Inception, bumping you back up ever-closer to where you came from. The base case is the part of the recursive problem that is not defined in terms of itself.</p>
+<p>“What are the base cases?” should be the first question you ask every time you want to approach a problem recursively. Many recursive problems have only one base case, but others have more.</p>
+<p><strong><em>Here is a short video about recursion</em> -&nbsp;<a href="https://vimeo.com/24716767">Ruby kickstart - Introduction to recursion</a>.</strong></p>
+<p>Implement a recursive version of the factorial function called <code class="inline">factorial_recursive</code>. It should accept any non-negative integer as input. Don’t worry about handling bad input.</p>
+<p>What’s the largest value your recursive factorial function can take as its input before it crashes? What does the error mean?</p>
 </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,13 +1,27 @@
 require 'rspec'
-require_relative 'binary_search'
+require_relative 'factorial'
+describe "factorial.rb" do
+  describe "#factorial_iterative(8)" do
+    it "should return 40320" do
+      expect(factorial_iterative(8)).to eq 40320
+    end
 
-describe "binary_search.rb" do
-  describe "#binary_search(56, (1..200).to_a)" do
-    let(:target) { 56 }
-    let(:array) { (1..200).to_a }
+    it "should not call itself within itself" do
+      expect(self).to receive(:factorial_iterative).once.and_call_original
+      factorial_iterative(8)
+    end
+  end
 
-    it "should return 55" do
-      expect(binary_search(target, array)).to eq 55
+  describe "#factorial_recursive(6)" do
+    it "should return 720" do
+      expect(factorial_recursive(6)).to eq 720
+    end
+
+    it "should call itself within itself" do
+      expect(self).to receive(:factorial_recursive).with(6).and_call_original
+      # provide 120 return after intercepting call
+      expect(self).to receive(:factorial_recursive).with(5) { 120 }
+      factorial_recursive(6)
     end
   end
 end
