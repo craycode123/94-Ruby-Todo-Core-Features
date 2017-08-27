a32ab34c2d70e0ceb1d67d15cb4756bf99fae4c5
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:37:42 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,43 +1,79 @@
-<div><p>In this challenge, we will start creating some data structures and have a better understanding on what’s going on under the hood with arrays and hashes. A powerful way to create an array is using the <code class="inline">Array.new</code> class method.</p>
-<pre><code class="ruby hljs">first_25_letters = (<span class="hljs-string">'A'</span>..<span class="hljs-string">'Z'</span>).first(<span class="hljs-number">25</span>)
+<div><p>We’re going to create a basic Boggle board generator. Read about <a href="http://en.wikipedia.org/wiki/Boggle">Boggle on Wikipedia</a>&nbsp;or <a href="http://www.wordplays.com/boggle">try out the game</a> if you’re not familiar with it.</p>
+<p>Boggle is a simple game, but when you get down into it there are some subtleties.</p>
+<p>First, we’re only going to model the first part of Boggle, where you place the Boggle dice and shake them around to create the initial board. To imagine what that might look like, visualize a blank 4x4 grid with a button next to it. When you push the button a letter appears in each of the 16 cells and you can begin playing Boggle.</p>
+<p>We’re going to do it in two steps. First, we’ll to build a dumb version which doesn’t care at all about whether it’s likely that the Boggle board will have English words. Second, we’ll build a version that models the dice.</p>
+  <h3>Tasks</h3>
+  <p><strong>Please note:</strong> Start with sample code from the gist given in the challenge. It gives a hint on class structure and internal methods.</p>
+  <h4>1. Stupid Boggle Board</h4>
+  <p>Our <code class="inline">BoggleBoard</code> class has one core instance method: <code class="inline">shake!</code></p>
+  <p>For the first step, focus on how you represent the board. <code class="inline">shake!</code> should modify the board, filling each cell with a random upper-case letter <code class="inline">A-Z</code>.</p>
+  <p>There are no other restrictions on the letters. They can appear multiple times, for example. Just pick a flippin’ random letter and don’t sweat it, ok?</p>
+  <p>I also know you’re worrying about how “Q” is always “Qu” in Boggle. Just let it be “Q” for now.</p>
+  <p>We want to write code that works like this:</p>
+  <pre><code class="ruby hljs">board = BoggleBoard.new
+  puts board
+  <span class="hljs-comment"># An unshaken board prints out something sensible, like:</span>
+  <span class="hljs-comment"># ____</span>
+  <span class="hljs-comment"># ____</span>
+  <span class="hljs-comment"># ____</span>
+  <span class="hljs-comment"># ____</span>
 
-letter_grid = Array.new(<span class="hljs-number">5</span>) { first_25_letters.shift(<span class="hljs-number">5</span>) }
-<span class="hljs-comment"># =&gt; [["A", "B", "C", "D", "E"],</span>
-<span class="hljs-comment"># ["F", "G", "H", "I", "J"],</span>
-<span class="hljs-comment"># ["K", "L", "M", "N", "O"],</span>
-<span class="hljs-comment"># ["P", "Q", "R", "S", "T"],</span>
-<span class="hljs-comment"># ["U", "V", "W", "X", "Y"] ]</span></code></pre>
-<p>Do you understand how the above code is working? If not, make a note to come back later so you understand passing blocks and the <code class="inline">shift</code> method.</p>
-<h3>Tasks</h3>
-<h4>1. Tic-Tac-Toe Revisited - Populating a Board</h4>
-<p>Take a look at the simple 2 dimensional array for Tic-Tac-Toe from <a href="https://code.nextacademy.com/courses/web-development-bootcamp/challenges/data-drill-nested-arrays">the previous challenge</a>. Let’s say we wanted to generate 10 sample boards with x’s and o’s. How would you do that? How do you make the x’s and o’s random? In the first iteration, it’s OK to create boards that don’t have logical ratio of x’s and o’s.</p>
-    <p>Write some driver code with a test to make sure your method is working, and your resultant array has 9 elements. What <code class="inline">Array</code> method would you use?</p>
-    <p><strong>Extra Credit:</strong> Go ahead and populate a board with a realistic ratio of x’s and o’s. There should only be either 5 x’s or 4 o’s or vice versa. There’s an enumerable method that works on arrays that can help you! How could you write a good driver code test to make sure the array has the right number of x’s and o’s?</p>
-    <h4>2 Convert nested array to hash</h4>
-    <p>Ruby provides a handy <code class="inline">Hash::[]</code> method which will convert a properly formatted array into a hash. <a href="http://www.ruby-doc.org/core-1.9.3/Hash.html#method-c-5B-5D">Read the documentation</a>.</p>
-    <p>Convert each entry (player) from the data table array from the previous challenge (shown below) into a hash. You will need to write code that modifies the original array into the right format that the <code class="inline">Hash::[]</code> method expects to get the proper result.</p>
-    <pre><code class="ruby hljs">roster = [[<span class="hljs-string">"Number"</span>, <span class="hljs-string">"Name"</span>, <span class="hljs-string">"Position"</span>, <span class="hljs-string">"Points per Game"</span>],
-                                           [<span class="hljs-number">12</span>, <span class="hljs-string">"Joe Schmo"</span>, <span class="hljs-string">"Center"</span>, [<span class="hljs-number">14</span>, <span class="hljs-number">32</span>, <span class="hljs-number">7</span>, <span class="hljs-number">0</span>, <span class="hljs-number">23</span>] ],
-                                           [<span class="hljs-number">9</span>, <span class="hljs-string">"Ms. Buckets"</span>, <span class="hljs-string">"Point Guard"</span>, [<span class="hljs-number">19</span>, <span class="hljs-number">0</span>, <span class="hljs-number">11</span>, <span class="hljs-number">22</span>, <span class="hljs-number">0</span>] ],
-                                           [<span class="hljs-number">31</span>, <span class="hljs-string">"Harvey Kay"</span>, <span class="hljs-string">"Shooting Guard"</span>, [<span class="hljs-number">0</span>, <span class="hljs-number">30</span>, <span class="hljs-number">16</span>, <span class="hljs-number">0</span>, <span class="hljs-number">25</span>] ],
-                                           [<span class="hljs-number">7</span>, <span class="hljs-string">"Sally Talls"</span>, <span class="hljs-string">"Power Forward"</span>, [<span class="hljs-number">18</span>, <span class="hljs-number">29</span>, <span class="hljs-number">26</span>, <span class="hljs-number">31</span>, <span class="hljs-number">19</span>] ],
-                                           [<span class="hljs-number">22</span>, <span class="hljs-string">"MK DiBoux"</span>, <span class="hljs-string">"Small Forward"</span>, [<span class="hljs-number">11</span>, <span class="hljs-number">0</span>, <span class="hljs-number">23</span>, <span class="hljs-number">17</span>, <span class="hljs-number">0</span>] ]]</code></pre>
-    <h4>3. Break It Down</h4>
-    <p>Take a moment to read up in the <a href="http://www.ruby-doc.org/core-1.9.3/Hash.html#method-c-5B-5D">documentation</a>&nbsp;to see the 3 formats that the <code class="inline">Hash::[]</code> method accepts.</p>
-    <p>Which format seems most convenient, given the nested array shown above? Test out the <code class="inline">Hash::[]</code> method with some test data in <code class="inline">irb</code>. How does your nested array need to change so it is the right format?</p>
-    <p>Create a method that can converts the nested array appropriately:</p>
-    <pre><code class="ruby hljs"><span class="hljs-function"><span class="hljs-keyword">def</span> <span class="hljs-title">convert_roster_format</span><span class="hljs-params">(roster)</span></span>
-    <span class="hljs-comment"># your code here</span>
-    <span class="hljs-comment"># should return a new format of roster which is an array of hashes</span>
-    <span class="hljs-keyword">end</span>
+  <span class="hljs-comment"># Shake (and modify) the board</span>
+  board.shake!
 
-    hashed_roster = convert_roster_format(roster)
+  puts board
+  <span class="hljs-comment"># Prints out:</span>
+  <span class="hljs-comment"># DUMV</span>
+  <span class="hljs-comment"># KSPD</span>
+  <span class="hljs-comment"># HCDA</span>
+  <span class="hljs-comment"># ZOHG</span>
 
-    <span class="hljs-comment"># Now, you should have an array of hash objects, which will allow you to write driver code like this:</span>
+  board.shake!
+  puts board
 
-    hashed_roster[<span class="hljs-number">2</span>]
-    <span class="hljs-comment"># =&gt; { "Number" =&gt; 31, "Name" =&gt; "Harvey Kay", "Position" =&gt; "Shooting Guard", "Points per Game" =&gt; [0, 30, 16, 0, 25] }</span>
-
-    puts hashed_roster[<span class="hljs-number">0</span>][<span class="hljs-string">"Name"</span>] == <span class="hljs-string">"Joe Schmo"</span> <span class="hljs-comment"># outputs true</span></code></pre>
-    <p>Write some more driver code to test your new array of hashes!</p>
-    </div>
+  <span class="hljs-comment"># We've shaken again, so a new random board:</span>
+  <span class="hljs-comment"># QIRZ</span>
+  <span class="hljs-comment"># EEBY</span>
+  <span class="hljs-comment"># OEJE</span>
+  <span class="hljs-comment"># MHCU</span></code></pre>
+  <h4>2. Smart(er) Boggle Board</h4>
+  <p>We need to model the dice, now. Think carefully about how shaking a Boggle board works. Each die lands in one and only one cell, with one side facing up.</p>
+  <p>Can you think of a way to model a die landing in only one cell without explicitly keeping track of which dice have landed and which haven’t? One way to do this is using a secondary <code class="inline">Array</code>, can you think of another?</p>
+    <p>We’ll still only have one core method, <code class="inline">BoggleBoard#shake!</code>. Here’s a list of Boggle dice, with “Q” representing “Qu”:</p>
+    <pre><code class="ruby hljs">AAEEGN
+    ELRTTY
+    AOOTTW
+    ABBJOO
+    EHRTVW
+    CIMOTU
+    DISTTY
+    EIOSST
+    DELRVY
+    ACHOPS
+    HIMNQU
+    EEINSU
+    EEGHNW
+    AFFKPS
+    HLNNRZ
+    DEILRX</code></pre>
+    <h4>3. Dealing with Q</h4>
+    <p>Now let’s take care of the “Q”. Assuming we want “Qu” to be printed rather than “Q”, how could we make that happen?</p>
+    <p>There are several ways of making this happen, especially if you keep in mind that how the board appears to the computer - how it’s represented in your program - doesn’t have to be how it appears to the person using the program.</p>
+    <p>Consider a few ways to make “Qu” print instead of just “Q”, deliberate on the tradeoffs for a few minutes, and implement one. You’ll probably want to change how the board is printed, too, since “Qu” will throw everything out of alignment.</p>
+    <p>For example, something like this might be appropriate:</p>
+    <pre><code class="ruby hljs">&gt; puts board.shake!
+    U N O T
+    S E W G
+    S V L T
+    L Qu C F</code></pre>
+    <p>Check out the <a href="http://www.ruby-doc.org/core-2.1.2/String.html#method-i-ljust">String#ljust</a>&nbsp;method for an easy way to add the right amount of space for display.</p>
+    <h3>As you’re coding, ask yourself…</h3>
+    <ul>
+    <li>Do I have a clear understanding of how this procedure works?
+    </li>
+    <li>Am I stuck because I know what I want to do but don’t know how to say it in Ruby?
+      </li>
+      <li>Am I stuck because my understanding of how Boggle works is too fuzzy or mistaken?
+      </li>
+      </ul>
+      </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,51 +1,25 @@
 require 'rspec'
-require_relative 'nested_arrays'
-describe "nested_arrays.rb" do
-  describe "#generate_realistic_tic_tac_toe" do
-    it "has the right ratio of 'x' and 'o' (both lowercase letters)" do
-      result = generate_realistic_tic_tac_toe.flatten.map(&:downcase)
-      expect(result.length).to eq(9)
-      expect(result.count('x')).to be >=(4)
-      expect(result.count('o')).to be >=(4)
+require_relative 'boggle_board'
+describe "BoggleBoard" do
+  let(:board) { BoggleBoard.new }
+
+  describe "#initialize" do
+    it "should declare variable @board with 4x4 nested array" do
+      @board = board.instance_variable_get(:@board)
+      expect(@board).to be_an Array
+      expect(@board.size).to eq 4
+      expect(@board[0]).to be_an Array
+      expect(@board[0].size).to eq 4
     end
   end
 
-  describe "#convert_roster_format" do
-    it "converts the nested array into an array of hashes" do
-      roster =
-      [
-        ["Number", "Name", "Position", "Points per Game"],
-        [12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
-        [9, "Ms. Buckets", "Point Guard", [19, 0, 11, 22, 0]],
-        [31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-        [7, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
-        [22, "MK DiBoux", "Small Forward", [11, 0, 23, 17, 0]]
-      ]
-
-      convert_roster_format(roster)[0]["Number"] = 12
-      convert_roster_format(roster)[0]["Name"] = "Joe Schmo"
-      convert_roster_format(roster)[0]["Position"] = "Center"
-      convert_roster_format(roster)[0]["Points per Game"] = [14, 32, 7, 0, 23]
-
-      convert_roster_format(roster)[1]["Number"] = 9
-      convert_roster_format(roster)[1]["Name"] = "Ms. Buckets"
-      convert_roster_format(roster)[1]["Position"] = "Point Guard"
-      convert_roster_format(roster)[1]["Points per Game"] = [19, 0, 11, 22, 0]
-
-      convert_roster_format(roster)[2]["Number"] = 31
-      convert_roster_format(roster)[2]["Name"] = "Harvey Kay"
-      convert_roster_format(roster)[2]["Position"] = "Shooting Guard"
-      convert_roster_format(roster)[2]["Points per Game"] = [0, 30, 16, 0, 25]
-
-      convert_roster_format(roster)[3]["Number"] = 7
-      convert_roster_format(roster)[3]["Name"] = "Sally Talls"
-      convert_roster_format(roster)[3]["Position"] = "Power Forward",
-        convert_roster_format(roster)[3]["Points per Game"] = [18, 29, 26, 31, 19]
-
-      convert_roster_format(roster)[4]["Number"] = 22
-      convert_roster_format(roster)[4]["Name"] = "MK DiBoux"
-      convert_roster_format(roster)[4]["Position"] = "Small Forward"
-      convert_roster_format(roster)[4]["Points per Game"] = [11, 0, 23, 17, 0]
+  describe "#shake!" do
+    it "should fill @board with random upper-case alphabets A-Z" do
+      board.shake!
+      @board = board.instance_variable_get(:@board)
+      x = rand(0..3)
+      y = rand(0..3)
+      expect( ("A".."Z").to_a ).to include @board[x][y]
     end
   end
 end
