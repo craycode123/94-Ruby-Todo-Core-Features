b2cd9e9f8e0b4d2b2d6b503193bbcee2e38f60ad
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:20:03 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,22 +1,37 @@
-<div><p>We’re going to write a method called <code class="inline">prime_factors</code> which returns the prime factors of an integer.</p>
-<p><strong>Prime factors</strong> of a positive integer are the prime numbers that divide that integer exactly. A number is <strong>prime</strong> if it is greater than 1 and no number divides it other than 1 and itself. <a href="http://en.wikipedia.org/wiki/Euclid">Euclid</a>, circa 300BC, proved that every integer greater than 1 is either prime itself or the product of prime numbers, and that these <strong>prime factors</strong> are unique.</p>
-<p>For example,</p>
-<p><img src="https://s3-ap-southeast-1.amazonaws.com/next-academy-learning-portal/static_assets/images/(public)+prime-factor.png" alt=""></p>
-<p>This fact is important enough to be called the <a href="http://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic">Fundamental Theorem of Arithmetic</a>.</p>
-<p>Prime factors play a major role in encryption systems. Check out <a href="https://www.youtube.com/watch?v=56fa8Jz-FQQ">this cool video</a> on how prime factors are used to encrypt your bank details!&nbsp;</p>
-<h3>Task</h3>
-<h4>Write a <code class="inline">prime_factors</code> method</h4>
-<p>Write a method <code class="inline">prime_factors</code> that takes an integer <code class="inline">n</code> and returns an array of the prime factors of <code class="inline">n</code>.</p>
-<p>A prime number can only be divided by itself and “1”. (Keep in mind that “1” is not considered a prime number - see below for examples of expectations).</p>
-<pre><code class="ruby hljs">prime_factors(<span class="hljs-number">3</span>) <span class="hljs-comment"># =&gt; [3]</span>
-prime_factors(<span class="hljs-number">6</span>) <span class="hljs-comment"># =&gt; [2,3]</span>
-prime_factors(<span class="hljs-number">8</span>) <span class="hljs-comment"># =&gt; [2,2,2]</span>
-prime_factors(<span class="hljs-number">25</span>) <span class="hljs-comment"># =&gt; [5,5]</span>
-prime_factors(<span class="hljs-number">123123123</span>) <span class="hljs-comment"># =&gt; [3, 3, 41, 333667]</span></code></pre>
-<p>Use pseudocode! Make sure you are clear on how you would calculate the prime factors yourself, and use this as a process to guide your code.</p>
-<h3>External Resources</h3>
-<ul>
-<li><a href="http://en.wikipedia.org/wiki/Prime_number">Prime Number on Wikipedia</a>
+<div><p>In computer science, there are many different ways to search through collections of data. Understanding the various algorithms and patterns for searching and sorting data sets is an essential part of computer science.</p>
+<p>Ruby abstracts many of the lower-level functions of programming by providing us with methods like <code class="inline">Array#index</code>. You don’t need to know <em>how</em> the index of an element is found, you just need to know <em>what</em> it is. In general, the fact that Ruby provides these abstractions is a Good Thing.</p>
+<p>However, it is also useful to understand that there are many different ways to perform a simple task like finding the index of an element in a list. Aside from being important to your knowledge of theory, the searching and sorting algorithms you use will have a significant impact on your program’s performance (how long it takes to execute and how much memory it uses).</p>
+<p>One of the most basic searching algorithms is the <strong>linear search</strong> . In this challenge, you will build a <code class="inline">linear_search</code> method using very basic programming constructs in Ruby. Here is a <a href="https://scratch.mit.edu/projects/72166092/">little game</a> for you play with to understand linear search better.</p>
+<p>Linear search (also called <em>sequential</em> search) is an algorithm for finding a particular value in a list by checking every one of its elements, one at a time and in sequence, until the desired one is found.</p>
+<p><em>Sources:&nbsp;
+</em></p>
+<ol>
+<li><em><a href="http://en.wikipedia.org/wiki/Linear_search">Wikipedia</a>
+</em>
 </li>
+<li><em><a href="http://pragprog.com/book/gwpy/practical-programming">Practical Programming</a>
+</em>
+</li>
+</ol>
+<h3>Task</h3>
+<h4>1. Basic linear search</h4>
+<p>Write a method <code class="inline">linear_search</code> that takes two arguments: an object and an array.</p>
+<ul>
+<li>It should return the index of the object in the array by going through each element in sequence and returning the index of the first instance of the element.
+  </li>
+<li>If the object searched for does not exist in the array, it should return <code class="inline">nil</code>.
+  </li>
 </ul>
+<pre><code class="ruby hljs">random_numbers = [<span class="hljs-number">6</span>, <span class="hljs-number">29</span>, <span class="hljs-number">18</span>, <span class="hljs-number">2</span>, <span class="hljs-number">72</span>, <span class="hljs-number">19</span>, <span class="hljs-number">18</span>, <span class="hljs-number">10</span>, <span class="hljs-number">37</span>]
+linear_search(<span class="hljs-number">18</span>, random_numbers)
+<span class="hljs-comment"># =&gt; 2</span>
+linear_search(<span class="hljs-number">9</span>, random_numbers)
+<span class="hljs-comment"># =&gt; nil</span></code></pre>
+<p>Here’s the catch: you can’t use any of Ruby’s built-in <code class="inline">Array</code> or <code class="inline">Enumerable</code> methods <em>except</em> for <code class="inline">Array#[]</code>. You will have to use of the keywords <code class="inline">for</code>, <code class="inline">while</code>, or <code class="inline">until</code>.</p>
+<h4>2. Global linear search</h4>
+<p>Write a new method <code class="inline">global_linear_search</code> that returns an array of <em>all the indices</em> for the object it searches for. In other words, if the object <code class="inline">x</code> is in more than one place in the array, <code class="inline">global_linear_search</code> will return an array containing the index of each occurrence of <code class="inline">x</code>.</p>
+<pre><code class="ruby hljs">bananas_arr = <span class="hljs-string">"bananas"</span>.split(<span class="hljs-regexp">//</span>)
+<span class="hljs-comment"># =&gt; ["b", "a", "n", "a", "n", "a", "s"]</span>
+global_linear_search(<span class="hljs-string">"a"</span>, bananas_arr)
+<span class="hljs-comment"># =&gt; [1, 3, 5]</span></code></pre>
 </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,15 +1,25 @@
 require 'rspec'
-require_relative 'prime_factors'
-describe "prime_factors.rb" do
-  describe "#prime_factors(8)" do
-    it "should return [2,2,2]" do
-      expect(prime_factors(8)).to eq [2, 2, 2]
+require_relative 'linear_search'
+
+describe "linear_search.rb" do
+  let(:random_numbers) {[6, 29, 18, 2, 72, 19, 18, 10, 37]}
+  let(:bananas_arr){ ["b", "a", "n", "a", "n", "a", "s"] }
+
+  describe "#linear_search(29, random_numbers)" do
+    it "should return 1" do
+      expect(linear_search(29, random_numbers)).to eq 1
     end
   end
 
-  describe "#prime_factors(6760)" do
-    it "should return [2, 2, 2, 5, 13, 13]" do
-      expect(prime_factors(6760)).to eq [2, 2, 2, 5, 13, 13]
+  describe "#linear_search(40, random_numbers)" do
+    it "should return nil" do
+      expect(linear_search(40, random_numbers)).to eq nil
+    end
+  end
+
+  describe "global_linear_search(\"n\", bananas_arr)" do
+    it "should return [2, 4]" do
+      expect(global_linear_search("n", bananas_arr)).to eq [2, 4]
     end
   end
 end
