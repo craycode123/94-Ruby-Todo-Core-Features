d0e3f26f3c871551697f11d6d062845230ec7247
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 14:01:35 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,39 +1,31 @@
-<div><p>Any Ruby program you write is running inside another pieces of software: the Ruby interpreter. And that interpreter is itself running inside another piece of software: your operating system. These software layers “outside” your Ruby program are called <strong>the environment</strong> and there are many ways you can shuttle data between the environment and your program.</p>
-<p>One way is via the <code class="inline">ARGV</code> constant which comes pre-defined in every Ruby program. It is an <code class="inline">Array</code> of <code class="inline">Strings</code> representing the command line arguments. Consider this simple program:</p>
-<pre><code class="ruby hljs">puts <span class="hljs-string">"The command line arguments are:"</span>
-puts ARGV.inspect
-puts <span class="hljs-string">""</span></code></pre>
-<p>The output looks like this:</p>
-<pre><code class="bash hljs ruby">$ ruby argv.rb these are
-The command line arguments <span class="hljs-symbol">are:</span>
-[<span class="hljs-string">"these"</span>, <span class="hljs-string">"are"</span>]
-
-$ ruby argv.rb these are the command line arguments
-The command line arguments <span class="hljs-symbol">are:</span>
-[<span class="hljs-string">"these"</span>, <span class="hljs-string">"are"</span>, <span class="hljs-string">"the"</span>, <span class="hljs-string">"command"</span>, <span class="hljs-string">"line"</span>, <span class="hljs-string">"arguments"</span>]
-
-$ ruby argv.rb <span class="hljs-string">"this come in as one"</span> <span class="hljs-string">"this comes in as two"</span>
-The command line arguments <span class="hljs-symbol">are:</span>
-[<span class="hljs-string">"this come in as one"</span>, <span class="hljs-string">"this comes in as two"</span>]
-
-$</code></pre>
+<div><p>When two processes on a single computer or two computers across a network want to share information they have to pre-agree on a format for exchanging data. A program running on my laptop can’t directly access the memory of some program running on yours, so they translate their internal state to/from this intermediate format first. <strong>XML, JSON, CSV, and YAML are all examples of text-based data exchange formats.</strong></p>
+<p>You can think of it like a dial-up modem. Because phone lines were designed to transmit audio signals, my modem translates all outgoing data into an audio signal before it’s sent over the phone line. Your modem receives the audio signal and translates it back into data your computer can make sense of.</p>
+<p>If heaven forbid you’ve never heard what this sounds like, <a href="http://www.youtube.com/watch?v=GSRG0TqxLWc">enjoy this video</a>. That’s the sound of <strong>DATA</strong> .</p>
+<p>You’ll be receiving, transforming, and forwarding data like this all the time in every program you write, so it’s important to get used to it. We’re going to start with a CSV file.</p>
 <h3>Objectives</h3>
-<h4>ARGV-ify Pig Latin</h4>
-<p>Take your pig latin script and augment it so you can do the following:</p>
-  <pre><code class="bash hljs ruby">$ ruby pig_latin.rb i love baseball
-  i ovelay aseballbay
-  $</code></pre>
-  <p>Save your file under <code class="inline">pig_latin.rb</code> and check if it passes the tests!</p>
-  <h4>ARGV-ify An ATM</h4>
-  <p>Now that you are more acquainted with ARGV, let’s ARGV-ify an ATM. In your code, set your bank balance to be 10 dollars. Then using ARGV, allow the user to <em>add</em>, <em>withdraw</em>, and <em>check</em> bank balance. An example is as follows:</p>
-  <pre><code class="bash hljs ruby">$ ruby atm.rb add <span class="hljs-number">10</span>
-  Your balance is now <span class="hljs-number">20</span> dollars.</code></pre>
-  <p>Note that you will now need to pass in two arguments in the command line. How would you do that? By the way, for this exercise, don’t worry about using classes. Just keep it simple.</p>
-    <p>Save your file under <code class="inline">atm.rb</code> and check if it passes the tests!</p>
-    <h4>ARGV-ify A Calculator</h4>
-    <p>Make a simple calculator&nbsp;script that can perform the 4 basic math operations (+, -, x, /) and augment it so you can do the following:</p>
-                                                                                          <pre><code class="bash hljs ruby">$ ruby calc.rb <span class="hljs-number">3</span> + <span class="hljs-number">4</span>
-                                                                                          <span class="hljs-number">7</span></code></pre>
-                                                                                          <p>Note, you’ll have to pass in the arguments in quotes because some characters have special meaning on the command-line. <code class="inline">*</code> for example means “every file in the current directory.” Probably not what you want to pass in as command-line arguments!</p>
-                                                                                          <p><em>Extra Credit:</em> instead of building a simple calculator, build an <a href="https://en.wikipedia.org/wiki/Reverse_Polish_notation">RPN calculator</a>!</p>
-                                                                                          </div>
+<p>The overall objective here is to learn to <strong>manipulate CSV data and objects as part of a single application</strong>. This is a common pattern in software engineering as per the above: change the representation of data from format A to format B to make it easier to do X with it.</p>
+  <p>Consider, for example, that trying to manipulate CSV data as pure text could get pretty tricky. Say you wanted to filter out a list of first names from the CSV file that started with the letter “A” or even find all people who had similar last names in area code 415 — how would you do that?</p>
+    <p>What if the contents of the CSV file was instead <strong>represented as objects</strong> in your program. Couldn’t you then just use <code class="inline">Enumerable#select</code> or similar methods to grab just the records that matched your search criteria?</p>
+    <p>These next few steps will walk you through the core part of this process — getting data out of the file and turning it into a bunch of objects you can manipulate in your program and then save them back to the file.</p>
+    <h4>Person and PersonParser</h4>
+    <p>First, make sure you have a copy of the CSV file from the gist. It contains a few hundred rows of random data about people.</p>
+    <p>Create a <code class="inline">Person</code> class such that each row of the CSV file represents a particular instance of the <code class="inline">Person</code> class. What attributes should a <code class="inline">Person</code> object have?</p>
+    <p>Create a <code class="inline">PersonParser</code> class that is initialized with the name of a CSV file. Then use Ruby’s&nbsp;<a href="http://ruby-doc.org/stdlib-1.9.2/libdoc/csv/rdoc/CSV.html">built-in CSV class</a>&nbsp;to implement a <code class="inline">PersonParser#people</code> method that returns an <code class="inline">Array</code> of properly-parsed <code class="inline">Person</code>s, based on the data in the CSV file. Say that three times fast.</p>
+    <p><em>Note: CSV is a “built-in” class, but you still need to <code class="inline">require</code> it at the top of your program.</em></p>
+    <h4>Manipulating in Ruby, Saving to CSV</h4>
+    <p>Create a <code class="inline">PersonParser#add_person</code> method which takes a new <code class="inline">Person</code> instance as its input and appends that instance to the parser’s internal <code class="inline">@people</code> array.</p>
+    <p>Next, create a <code class="inline">PersonParser#save</code> method which uses the CSV class to save the current state of the parser to a new CSV file. For example:</p>
+    <pre><code class="ruby hljs">parser = PersonParser.new(<span class="hljs-string">'people.csv'</span>)
+    parser.add_person( Person.new(...) )
+
+    <span class="hljs-comment"># This will now write to people.csv, but there will be one more row</span>
+    <span class="hljs-comment"># corresponding to the extra Person you just added.</span>
+    parser.save
+    </code></pre>
+    <p>Note: When you read and write to a file, you can choose a format (like “r” for read and “w” for write). The CSV formats are the same as the <a href="http://ruby-doc.org/core-1.9.3/IO.html">file formats</a>.</p>
+    <h4>Translating from CSV-land to Ruby-land</h4>
+    <p>Because text-based data formats don’t know anything about where your data is going to be used, there aren’t easy ways to encode language-specific features into the format. For example, the <code class="inline">people.csv</code> file has a <code class="inline">created_at</code> field. In a CSV this is just a conveniently-formatted string, but in Ruby we might want it to be an instance of the <code class="inline">DateTime</code> class.</p>
+    <p>At the top of your Ruby program add the line:  </p>
+    <pre><code class="ruby hljs"><span class="hljs-keyword">require</span> <span class="hljs-string">'date'</span></code></pre>
+    <p>Instead of storing <code class="inline">Person#created_at</code> as a <code class="inline">String</code>, use the <a href="http://www.ruby-doc.org/stdlib-1.9.3/libdoc/date/rdoc/DateTime.html#method-c-parse">DateTime#parse method</a>&nbsp;to parse the <code class="inline">String</code> into an actual honest-to-goodness <code class="inline">DateTime</code> object.</p>
+    </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,52 +1,59 @@
 require 'rspec'
-require_relative 'atm'
-require_relative 'calc'
-require_relative 'pig_latin'
+require_relative 'parser'
+describe "Person" do
+  let(:person) { Person.new(id: 201, first_name: "Jane", last_name: "Doe", email: "jane_doe@gmail.com", phone_number: "2020202") }
 
-describe "pig_latin.rb" do
-  context "when executing $ruby pig_latin.rb i love baseball" do
-    it "should print \"i ovelay aseballbay\"" do
-      regex = /i ovelay aseballbay/
-      expect( %x(ruby pig_latin.rb i love baseball) ).to match regex
-    end
-  end
-
-  context "when executing $ruby pig_latin.rb hello world" do
-    it "should print \"ellohay orldway\"" do
-      regex = /ellohay orldway/
-      expect( %x(ruby pig_latin.rb ellohay orldway) ).to match regex
-    end
+  it "should have attributes: id, first_name, last_name, email, phone_number, created_at" do
+    expect(person.id).not_to be_nil
+    expect(person.first_name).not_to be_nil
+    expect(person.last_name).not_to be_nil
+    expect(person.phone_number).not_to be_nil
+    expect(person.email).not_to be_nil
+    expect(person.created_at).not_to be_nil
   end
 end
 
-describe "calc.rb" do
-  context "when executing $ruby calc.rb \"3 + 4\"" do
-    it "should print \"7\"" do
-      regex = /7/
-      expect( %x(ruby calc.rb \"3 + 4\") ).to match regex
+describe "PersonParser" do
+  let(:parser) { PersonParser.new("people.csv") }
+  let(:person) { Person.new(id: 201, first_name: "Jane", last_name: "Doe", email: "jane_doe@gmail.com", phone_number: "2020202") }
+
+  describe "#people method" do
+    it "should return an array of Person objects" do
+      expect(parser.people).to be_an Array
+      random_person = parser.people.sample
+      expect(random_person).to be_a Person
+      expect(random_person.instance_variables).to eq [:@id, :@first_name, :@last_name, :@email, :@phone_number, :@created_at]
     end
   end
 
-  context "when executing $ruby calc.rb \"50 * 6\"" do
-    it "should print \"300\"" do
-      regex = /300/
-      expect( %x(ruby calc.rb \"50 * 6\") ).to match regex
+  describe "#add_person method" do
+    it "should take in a Person object" do
+      parser.people
+      expect{ parser.add_person }.to raise_error ArgumentError
+      expect{ parser.add_person(person) }.not_to raise_error
+    end
+
+    it "should increase @people array by 1" do
+      parser.people
+      initial_count = parser.people.size
+      parser.add_person(person)
+      current_count = parser.people.size
+      expect( current_count - initial_count ).to eq 1
     end
   end
-end
 
-describe "atm.rb" do
-  context "when executing $ruby atm.rb add 10" do
-    it "should print \"Your balance is now 20 dollars\"" do
-      regex = /Your balance is now 20 dollars/
-      expect( %x(ruby atm.rb add 10) ).to match regex
-    end
-  end
+  describe "#save method" do
+    it "should save all @people into csv file" do
+      original_rows = File.readlines("people.csv").size
+      parser.people
+      parser.add_person(person)
+      parser.save
+      current_rows = File.readlines("people.csv").size
+      expect(current_rows - original_rows).to eq 1
 
-  context "when executing $ruby atm.rb withdraw 10" do
-    it "should print \"Your balance is now 0 dollars\"" do
-      regex = /Your balance is now 0 dollars/
-      expect( %x(ruby atm.rb widthdraw 10) ).to match regex
+      # clean up so that there're no confusion for students
+      parser.people.delete_at(-1)
+      parser.save
     end
   end
 end
