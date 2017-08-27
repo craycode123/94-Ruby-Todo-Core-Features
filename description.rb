615ed276447cfc430243a5bc9b86f148469332e5
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:04:05 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,46 +1,8 @@
-<div><p>The fundamental way that programmers organize their code is by writing methods. Methods (the code you write between <code class="inline">def</code> and <code class="inline">end</code>) have input, output, and side-effects.</p>
-<p>A method’s input is the arguments you pass in, such as in <code class="inline">puts "hi mom"</code>, where the <code class="inline">puts</code> method’s input is the argument “hi mom”. The <code class="inline">puts</code> method takes input, but only ever returns <code class="inline">nil</code>as output. Its side-effect is that it writes to STDOUT. On the other hand, many methods have no side-effects and are purely input/output. For example, <code class="inline">File.exists?("app/models")</code> takes <code class="inline">"app/models"</code> as input, and returns <code class="inline">true</code> or <code class="inline">false</code>.</p>
-<p>One of the conventions that you’ll learn in Ruby is that methods with side-effects are appended with a <code class="inline">!</code> (what programmers call a “bang”). This helps future programmers calling these methods understand that when they call it, something serious is happening behind the scenes.</p>
-<p>For this exercise, you will write a function (a method with no side-effects), and then a method with the side-effect of “destroying” its input argument.</p>
-<p><em>Note: the bang <code class="inline">!</code> symbol is used because permanently modifying data is dangerous. Use destructive methods with caution.</em></p>
-<h3>Tasks</h3>
-<h4>Rules for your method</h4>
-<p>You will be writing two versions of a method called <code class="inline">destroy_message</code> which takes in a string identified by the variable <code class="inline">message</code> and modifies that string based upon the following rules:</p>
-<ul>
-<li><p>A message is a string that may or may not contain a <code class="inline">":"</code>.</p>
-</li>
-<li><p>If the original message contains a <code class="inline">":"</code> then all content after (but not including) the <code class="inline">":"</code> is removed when the message self-destructs. So if the original message is <code class="inline">"This message will self destruct: Ruby is fun"</code> it will then return <code class="inline">"This message will self destruct:"</code>.</p>
-</li>
-<li><p>If the original message does not contain a <code class="inline">":"</code> then it will not change when it is destructed.</p>
-</li>
-</ul>
-<h4>Writing methods with no side effects</h4>
-<p>Start by writing the <code class="inline">destroy_message</code> method following the rules outlined above. It <strong>must not</strong> permanently alter the original string passed in.</p>
-<p>What does that mean? Here is an example of a <em>non-destructive</em> method:</p>
-<pre><code class="ruby hljs">name = <span class="hljs-string">"Mr. Billy Jones"</span>
-
-<span class="hljs-comment"># The String#sub method replaces one part of the string</span>
-<span class="hljs-comment"># with another and returns the new, modified string.</span>
-name.sub(<span class="hljs-string">'Jones'</span>, <span class="hljs-string">'Jimmy Jones'</span>)
-<span class="hljs-comment"># =&gt; "Mr. Billy Jimmy Jones"</span>
-
-<span class="hljs-comment"># But it does NOT permanently change the string, as we</span>
-<span class="hljs-comment"># can see here:</span>
-name
-<span class="hljs-comment"># =&gt; "Mr. Billy Jones"</span>
-
-<span class="hljs-comment">### Destroying everything!</span></code></pre>
-<p>Now, write the method <code class="inline">destroy_message!</code> which does the same thing as <code class="inline">destroy_message</code> (without the <code class="inline">!</code>) <strong>except</strong> that this new method will permanently modify the original string.</p>
-<p>What does it mean to permanently modify a piece of data? Here is an example of a <em>destructive</em> method:</p>
-<pre><code class="ruby hljs">name = <span class="hljs-string">"Mr. Billy Jones"</span>
-
-<span class="hljs-comment"># The String#sub! method replaces one part of the ORIGINAL</span>
-<span class="hljs-comment"># string with another and returns the modified string.</span>
-name.sub!(<span class="hljs-string">'Jones'</span>, <span class="hljs-string">'Jimmy Jones'</span>)
-<span class="hljs-comment"># =&gt; "Mr. Billy Jimmy Jones"</span>
-
-<span class="hljs-comment"># When you call #sub! on a string, it permanently modifies</span>
-<span class="hljs-comment"># the original value.</span>
-name
-<span class="hljs-comment"># =&gt; "Mr. Billy Jimmy Jones"</span></code></pre>
-</div>
+<div><p>&nbsp;When you need to operate on a Ruby <code class="inline">Array</code>, you can accomplish just about anything you can imagine with the <code class="inline">each</code> method. Often, though, using <code class="inline">each</code> is not the best way to work with <code class="inline">Array</code>.</p>
+<p>For example, when you find yourself adding conditional logic inside your <code class="inline">each</code> block, or using <code class="inline">each</code> to search, sort, or transform the <code class="inline">Array</code>, consider the power of Ruby’s <code class="inline">Enumerable</code> module.</p>
+<p>Constraint: <strong>do not use the <code class="inline">for</code> or <code class="inline">each</code> method</strong> in any of your solutions to this challenge. Besides these two, any other enumerable method is up for grabs!</p>
+  <h3>Task</h3>
+  <h4>Utilize Enumerable’s methods</h4>
+  <p>Enumerable is full of useful, convenient methods that make working with enumerables much simpler. This exercise will expose you to a handful of these methods. Each method in the exercise tells you which Enumerable method you should use, so it’s your job to figure out how to call it to perform the desired task. &nbsp;</p>
+  <p>Note: Be careful not to break the driver code.</p>
+  </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,53 +1,52 @@
-describe "destructive_methods.rb" do
-  describe "#destroy_message" do
-    context "when arguement \"first_half: second_half\"" do
-      let(:arg){ "first_half: second_half" }
+require 'rspec'
+require_relative 'power_of_enumerables'
 
-      it "returns \"first_half:\"" do
-        expect(destroy_message(arg)).to eq "first_half:"
-      end
-    end
+describe "print_odd_indexed_integers([2, 4, 6, 8, 10, 12])" do
+  let(:action) { print_odd_indexed_integers([2, 4, 6, 8, 10, 12])  }
 
-    context "when arguement \"first_half\"" do
-      let(:arg){ "first_half" }
+  it "should print \"4\\n8\\n12\"" do
+    expect{ action }.to output(/4\n8\n12/).to_stdout
+  end
+end
 
-      it "returns \"first_half\"" do
-        expect(destroy_message(arg)).to eq arg
-      end
-    end
+describe "odd_integers([3, 4, 7, 9, 10, 16])" do
+  let(:action) { odd_integers([3, 4, 7, 9, 10, 16]) }
 
-    it "does not change the original string" do
-      arg = "temp: temp2"
-      string = arg.dup
-      destroy_message(arg)
-      expect(arg).to eq string
-    end
+  it "should return [3, 7, 9]" do
+    expect(action).to eq [3, 7, 9]
+  end
+end
+
+describe "first_under([13, 21, 7, 0, 11, 106], 10)" do
+  let(:action){ first_under([13, 21, 7, 0, 11, 106], 10) }
+
+  it "should return 7" do
+    expect(action).to eq 7
+  end
+end
+
+describe "add_bang([\"hi\", \"mom\"])" do
+  let(:action) { add_bang(["hi", "mom"]) }
+
+  it "should return [\"hi!\", \"mom!\"]" do
+    expect(action).to eq ["hi!", "mom!"]
+  end
+end
+
+describe "sum([1, 1, 2, 3, 5])" do
+  let(:action) { sum([1, 1, 2, 3, 5]) }
+
+  it "should return 12" do
+    expect(action).to eq 12
+  end
+end
+
+describe "sorted_triples(words)" do
+  let(:words) { %w(the salted pork is particularly good) }
+  let(:action) { sorted_triples(words) }
+
+  it "should return [[\"pork\", \"salted\", \"the\"], [\"good\", \"is\", \"particularly\"]]" do
+    expect(action).to eq [["pork", "salted", "the"], ["good", "is", "particularly"]]
   end
 
-  describe "#destroy_message!" do
-    context "when arguement \"first_half: second_half\"" do
-      let(:arg){ "first_half: second_half" }
-
-      it "it will change the original string to \"first_half:\"" do
-        string = arg.dup
-        destroy_message!(arg)
-        expect(arg).not_to eq string
-        expect(arg).to eq "first_half:"
-      end
-    end
-
-    context "when arguement \"first_half\"" do
-      let(:arg){ "first_half" }
-
-      it "returns nil" do
-        expect(destroy_message!(arg)).to eq nil
-      end
-
-      it "does not change the original string" do
-        string = arg.dup
-        destroy_message!(arg)
-        expect(arg).to eq string
-      end
-    end
-  end
 end
