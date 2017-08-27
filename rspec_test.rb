ce1461fd62997bfd78b4535c00f5a2ddc70100a7
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 12:15:45 +0800

    first commit

--- rspec_test.rb
+++ rspec_test.rb
@@ -1,15 +1,6 @@
-require 'rspec'
-require_relative 'manipulate'
-describe "manipulate.rb\:" do
-  describe "manipulate(\"hello\", \"e\")" do
-    it "should return \`hEllo\`" do
-      expect(manipulate("hello", "e")).to eq "hEllo"
-    end
-  end
-
-  describe "manipulate(\"hello\", \"l\")" do
-    it "should return \`heLlo\`" do
-      expect(manipulate("hello", "l")).to eq "heLlo"
-    end
+describe "code_style.rb" do
+  it "should not raise any errors" do
+    # if no errors in code, refactoring is considered successful
+    # consider implementing hound/rubocop to implement code style test?
   end
 end
