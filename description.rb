781d1473fa8f9685289d841ada3d8ac5f5ab333c
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:11:42 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,11 +1,39 @@
-<div><p>In this challenge, we will be writing methods and appropriate regular expressions to find, hide and format social security numbers. </p>
-<h3>Objectives</h3>
-<ol>
-<li><p>Use the driver code in the gist to guide you to a working solution.</p>
-</li>
-<li><p>Add additional driver code where you want to feel more comfortable with your implementation. For instance, what happens when you pass a string with only a single SSN into the <code class="inline">hide_all_ssns</code> or <code class="inline">get_all_ssns</code> methods?</p>
-</li>
-<li><p>Did adding driver code help you solve the challenge more effectively? How? (Answer in the gist as a comment)</p>
-</li>
-</ol>
+<div><p>Here’s a story every programmer knows. Your friend George comes up to you one day and asks, “I have an idea for a script, but I don’t want to write it. Will you, my talented programmer friend, do it for me?”</p>
+  <p>In this situation your job will involve:</p>
+  <ol>
+  <li>Understanding the picture George has in his head of what he wants built and why
+  </li>
+  <li>Creating various representations of what you think he wants, ranging from product specifications, to <a href="http://en.wikipedia.org/wiki/Website_wireframe">wireframes</a>&nbsp;and <a href="http://en.wikipedia.org/wiki/User_story">user stories</a>, to <a href="http://en.wikipedia.org/wiki/Pseudocode">pseudocode</a>&nbsp;used to communicate with other engineers.
+    </li>
+  <li>Implementing prototypes and iterating towards a finalized product.
+    </li>
+  </ol>
+  <p>When this process goes wrong, it ends up looking like this:</p>
+  <img src="https://s3-ap-southeast-1.amazonaws.com/code-division/learning_portal/static_assets/images/tree_comic.jpg" alt="tree_comic" width="570px" height="500px"><p>As a programmer, you will be expected to build code from all kinds of specifications:&nbsp;<a href="http://en.wikipedia.org/wiki/Website_wireframe">wireframes</a>,&nbsp;<a href="http://en.wikipedia.org/wiki/User_story">user stories</a>, and&nbsp;<a href="http://en.wikipedia.org/wiki/Pseudocode">pseudocode</a>. It’s important to be able to understand how to read these models and translate them into a functional program.</p>
+  <p>More importantly, you have to understand the value each model has. Pseudocode, for example, is primarily used to communicate the essence of an algorithm without getting bogged down in language-specific syntax. A good programmer can take well-written pseudocode and translate it into functional code in the language of his choice.</p>
+  <h3>Objectives</h3>
+  <h4>Build from Pseudocode</h4>
+  <p>In the prepwork, we talked about coding a pig latin program. Here’s a <a href="https://drive.google.com/file/d/0B9G51A7Ar4SlTVh0LWRIaG02eGc/view">refresher</a> for you in case you have forgotten about it. In this challenge, we will get our hands dirty and code up a pig latin programme. Below is the pseudocode for a <code class="inline">pig_latin</code> program that we would like you to code:</p>
+  <pre><code class="hljs">Script: CONVERT TO PIG LATIN
+
+  Iteration One: CONVERT SINGLE WORD
+
+  GET a word from user input
+  IF the word starts with a vowel, don't change it
+ELSE replace the word with its pig latin equivalent
+  GET all of the consonants before the first vowel in the word
+  SET the consonants at the end of the word and add the suffix "ay"
+ENDIF
+PRINT the pig-latin-ified word
+
+Iteration Two: CONVERT COMPLETE SENTENCE
+
+GET a sentence from user input
+FOR each word in the sentence
+  CONVERT SINGLE WORD
+ENDFOR
+PRINT the converted sentence
+DISPLAY the number of words converted</code></pre>
+<p>Write code that accomplishes the expectations laid out in the pseudocode above.&nbsp;</p>
+<p>ps: here’s a cute cartoon about <a href="https://www.youtube.com/watch?v=aggpWsIylFI">pig latin</a>. Imagine talking in “secret pig latin code” with your friends!</p>
 </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,74 +1,28 @@
 require 'rspec'
-require_relative "regular_expressions"
+require_relative 'pig_latin.rb'
 
-describe "regular_expressions.rb" do
-  let(:ssn){ "234-60-1422" }
-  let(:arg_with_ssn){ "please don't share this: #{ssn}" }
-  let(:arg_without_ssn){ "please confirm your identity: XXX-XX-1422" }
-  let(:string_with_ssns){ "234-60-1422, 350-80-0744, 013-60-8762" }
-  let(:ssn_regex){ /(\d{3})\D?(\d{2})\D?(\d{4})/ }
-
-  describe "#has_ssn?" do
-    it "returns true if it has what looks like a SSN" do
-      result = has_ssn?(arg_with_ssn)
-      expect(result).to eq true
-    end
-
-    it "returns false if it doesn't have a SSN" do
-      result = has_ssn?(arg_without_ssn)
-      expect(result).to eq false
+describe "pig_latin.rb" do
+  describe "convert_word_to_pig_latin(\"hello\")" do
+    it "should return \`ellohay\`" do
+      expect(convert_word_to_pig_latin("hello")).to eq "ellohay"
     end
   end
 
-  describe "#grab_ssn" do
-    it "returns an SSN if the string has an SSN" do
-      result = grab_ssn(arg_with_ssn)
-      expect(result).to eq ssn
-    end
-
-    it "returns nil if it doesn't have a SSN" do
-      result = grab_ssn(arg_without_ssn)
-      expect(result).to eq nil
+  describe "convert_word_to_pig_latin(\"aye\")" do
+    it "should return \`aye\`" do
+      expect(convert_word_to_pig_latin("aye")).to eq "aye"
     end
   end
 
-  describe "#grab_all_ssns" do
-    it "returns all SSNs if the string has any SSNs" do
-      result = grab_all_ssns(string_with_ssns)
-      expected_result = string_with_ssns.split(", ")
-      expect(result).to eq expected_result
-    end
-
-    it "returns an empty Array if it doesn't have any SSNs" do
-      result = grab_all_ssns(arg_without_ssn)
-      expect(result).to eq []
+  describe "convert_sentence_to_pig_latin(\"hello world\")" do
+    it "should return \"ellohay orldway\"" do
+      expect(convert_sentence_to_pig_latin("hello world")).to eq "ellohay orldway"
     end
   end
 
-  describe "#hide_all_ssns" do
-    it "obfuscates any SSNs in the string" do
-      result = hide_all_ssns(string_with_ssns)
-      expected_result = string_with_ssns.gsub(ssn_regex, 'XXX-XX-\3')
-      expect(result).to eq expected_result
-    end
-
-    it "does not alter a string without SSNs in it" do
-      result = hide_all_ssns(arg_without_ssn)
-      expect(result).to eq arg_without_ssn
-    end
-  end
-
-  describe "#format_ssns" do
-    let(:weird_ssn_string){ "234601422, 350.80.0744, 013-60-8762" }
-
-    it "finds and reformat any SSNs in the string" do
-      result = format_ssns(weird_ssn_string)
-      expect(result).to eq string_with_ssns
-    end
-
-    it "does not alter a string without SSNs in it" do
-      result = format_ssns(arg_without_ssn)
-      expect(result).to eq arg_without_ssn
+  describe "convert_sentence_to_pig_latin(\"aye sir\")" do
+    it "should return \"aye irsay\"" do
+      expect(convert_sentence_to_pig_latin("aye sir")).to eq "aye irsay"
     end
   end
 end
