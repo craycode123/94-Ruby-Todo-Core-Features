282108bdcf95e98af0cfe4f0a0553dcaf797ab57
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:08:37 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,18 +1,24 @@
-<div><p>We hope you are now more acquainted with error messages, data types, and how certain methods can only be used by certain data types. Let’s now put everything together and fix erroneous code.</p>
-<h3>Objectives</h3>
-<p>The exercises given contain bugs. Your task is to figure out what the error is and then change to code to remove the bug. In order to do this, you will have to read the error message that Ruby returns.</p>
-  <p>In the first exercise, you will get rid of the <code class="inline">ArgumentError</code> bug by changing the statement that <em>invokes</em> the method.</p>
-  <p>In the second exercise, you will have to change the method <em>definition</em> in order to solve the bug.</p>
-  <p>The third exercise contains a few <code class="inline">TypeError</code>s, which are common bugs in <a href="http://en.wikipedia.org/wiki/Type_system#Dynamic_typing">dynamically typed</a> languages such as Ruby.</p>
-  <p>Fix all the errors in the code in the repo&nbsp;to make it run successfully without changing it’s intended purpose!</p>
-  <p>By the way, here’s a juicy question: why would we rewrite the <em>invocation</em> as opposed to the <em>definition</em> of the method?</p>
-  <p>In a small program like this, the code is flexible enough to warrant either choice. When working on larger applications, you have to <strong>constantly be aware of <em>how</em> you are fixing bugs</strong>. Fixing one bug can easily cause more bugs or confusion down the line if you are not careful.</p>
-  <h3>Using rspec</h3>
-  <ol>
-  <li><p>Make sure your code works by running <code class="inline">ruby argument_error_1.rb</code>, <code class="inline">ruby type_error.rb</code>, <code class="inline">ruby argument_error_2.rb</code>.</p>
-  </li>
-  <li><p>Then run <code class="inline">rspec rspec_test.rb</code> in the terminal to check that the requirements given are met. </p>
-  </li>
-  </ol>
-  <p>If you see red words, it means that even though your code is working, you may have not done it the right way. Read the requirements again and edit your code accordingly. If you are still unable to pass all the tests, flag a mentor and ask him/her to help review your code.</p>
-  </div>
+<div><p>Ruby’s <code class="inline">String</code> class has over <a href="http://www.ruby-doc.org/core-2.1.3/String.html">100 public methods</a>&nbsp;that give Ruby programmers a remarkable power to process, manipulate, and transform textual data. About a dozen of those <code class="inline">String</code> methods use <strong>reg</strong>ular <strong>ex</strong>pressions (regex) in order to allow for high-powered string matching.</p>
+<p>An example of low-powered string matching is when you use your browser’s search (Ctrl-F on a Ubuntu/Linux) for a specific word, say, your own name. What if you wanted to search your browser for anything that matches the pattern for a social security number or a credit card number? For that, you’d use regular expressions.</p>
+<h4>Objectives</h4>
+<ol>
+<li><p>Do <a href="https://regexone.com/">this tutorial</a> to get familiar with pattern matching using regex.</p>
+</li>
+<li><p>Learn how to use <a href="http://rubular.com">http://rubular.com</a>&nbsp;to help you code your regex. </p>
+</li>
+<li><p>Figure out how <code class="inline">gsub</code> and the match operator <code class="inline">=~</code> are used in <code class="inline">regex_example.rb</code>. Remember to check the docs to see what <code class="inline">gsub</code> and <code class="inline">=~</code> will return.</p>
+</li>
+<li><p>Complete the exercises given in <code class="inline">regex_exercise.rb</code>. The questions are listed below as well:</p>
+<p>a. Write a method named <code class="inline">"hide_serial(string)"</code> that accepts a string as an argument that uses the regex for <code class="inline">"123456-12-1234"</code> and substitute the first two sections with <code class="inline">"X"</code> to get <code class="inline">"XXXXXX-XX-1234"</code>.</p>
+<p>b. Write a method named <code class="inline">"hide_digits(string)"</code> that accepts a string as an argument that substitutes the digits in the sentence <code class="inline">"You have 100 dollars"</code> with <code class="inline">"-"</code> to get <code class="inline">"You have --- dollars"</code>.</p>
+<p>c. Write a method named <code class="inline">"hide_last_four(string)"</code> that accepts a string as an argument that substitute the last 4 digits with <code class="inline">"*"</code> in this string: <code class="inline">"12-34-5678, 90-80-7012, 45-65-1234"</code> to get <code class="inline">"12-34-****, 90-80-****, 45-65-****"</code>.</p>
+</li>
+</ol>
+<h4>Further reading:</h4>
+<ol>
+<li><p><a href="http://www.ruby-doc.org/docs/ProgrammingRuby/html/language.html#UJ">The Pickaxe Guide</a>&nbsp;</p>
+</li>
+<li><p><a href="http://www.bluebox.net/about/blog/2013/02/using-regular-expressions-in-ruby-part-1-of-3/">ReGex Overview</a> </p>
+</li>
+</ol>
+</div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,73 +1,22 @@
-describe "argument_error_1.rb" do
-  before(:each) { load 'argument_error_1.rb' if File.exist?('argument_error_1.rb') }
-
-  it "should not raise any errors" do
-    expect{ run }.not_to raise_error
-  end
-
-  it "should not change the \"mean\" function" do
-    expect{ mean(5,3,6,10) }.to raise_error ArgumentError
-  end
-end
-
-describe "argument_error_2.rb" do
-  before(:each) { load 'argument_error_2.rb' if File.exist?('argument_error_2.rb') }
-
-  it "should not raise any errors" do
-    expect{ run }.not_to raise_error
-  end
-
-  it "should allow the \"mean\" method to accept multiple arguements" do
-
-    expect{ mean(5,6,7,8) }.not_to raise_error
-  end
-end
-
-describe "type_error.rb" do
-  before(:each){ load 'type_error.rb'  if File.exist?('type_error.rb')}
-  let(:words) { %w{ all i can say is that my life is pretty plain } }
-  let(:words_with_nil) { words.dup.insert(3, nil) }
-  let(:mixed_array) { ["2", 1, "5", 4, "3"] }
-
-  def custom_sort(array)
-    converted_array = array.map{ |elem| elem.nil? ? "nil" : elem.to_s }
-    converted_array.sort
-  end
-
-  it "should not raise any errors" do
-    expect{ run }.not_to raise_error
-  end
-
-  describe "#print_and_sort(words)" do
-    it "should print \"all i can say is that my life is pretty plain\"" do
-      regex = /all i can say is that my life is pretty plain/
-      expect{ print_and_sort(words) }.to output(regex).to_stdout
-    end
-
-    it "should sort words" do
-      expect( print_and_sort(words) ).to eq custom_sort(words)
+require 'rspec'
+require_relative 'regex_exercise'
+describe 'regexp' do
+  describe 'hide_serial' do
+    it 'should return value XXXXXX-XX-1234' do
+      string = '123456-12-1234'
+      expect(hide_serial(string)).to eq('XXXXXX-XX-1234')
     end
   end
-
-  describe "#print_and_sort(words_with_nil)" do
-    it "should print \"all i can nil say is that my life is pretty plain\"" do
-      regex = /all i can nil say is that my life is pretty plain/
-      expect{ print_and_sort(words_with_nil)}.to output(regex).to_stdout
-    end
-
-    it "should sort words_with_nil"do
-      expect( print_and_sort(words_with_nil) ).to eq custom_sort(words_with_nil)
+  describe 'hide_digits' do
+    it 'should replace all digits with - symbol' do
+      string = 'You have 100 dollars'
+      expect(hide_digits(string)).to eq('You have --- dollars')
     end
   end
-
-  describe "#print_and_sort(mixed_array)" do
-    it "should print \"2 1 5 4 3\"" do
-      regex = /2 1 5 4 3/
-      expect{ print_and_sort(mixed_array) }.to output(regex).to_stdout
-    end
-
-    it "should sort mixed_array" do
-      expect( print_and_sort(mixed_array) ).to eq custom_sort(mixed_array)
+  describe 'hide_last_four' do
+    it 'should replace last 4 digits of each group to ****' do
+      string = '12-34-5678, 90-80-7012, 45-65-1234'
+      expect(hide_last_four(string)).to eq("12-34-****, 90-80-****, 45-65-****")
     end
   end
 end
