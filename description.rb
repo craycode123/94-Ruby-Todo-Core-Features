86c502dd4e7b2d1bc48567c049e5648fb9ae0af7
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:33:50 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,34 +1,43 @@
-<div><p>Arrays are exceedingly useful for storing collections of data due to their flexibility and simplicity. The possibilities for different uses of arrays are limitless.</p>
-<p>One common design pattern you will encounter is <strong>nested arrays</strong>, i.e. arrays within arrays. With nested arrays, we can store and organize data according to a variety of patterns.</p>
-<pre><code class="ruby hljs">nested_array = [[<span class="hljs-number">1</span>, <span class="hljs-number">2</span>], [<span class="hljs-number">3</span>, <span class="hljs-number">4</span>], [<span class="hljs-number">5</span>, <span class="hljs-number">6</span>]]
-puts nested_array[<span class="hljs-number">1</span>][<span class="hljs-number">0</span>] == <span class="hljs-number">3</span> <span class="hljs-comment"># outputs true</span></code></pre>
-<h3>Example: Grid Pattern</h3>
-<p>Using nested arrays, we can represent a 2-dimensional grid of a tic tac toe board:</p>
-<pre><code class="ruby hljs">tic_tac_toe = [[<span class="hljs-string">"X"</span>, <span class="hljs-string">"X"</span>, <span class="hljs-string">"O"</span>],
-                                            [<span class="hljs-string">"O"</span>, <span class="hljs-string">"X"</span>, <span class="hljs-string">"O"</span>],
-                                            [<span class="hljs-string">"O"</span>, <span class="hljs-string">"O"</span>, <span class="hljs-string">"X"</span>]]</code></pre>
-<p>Any element in a grid pattern has two indices: the first index identifies the “row” (the position of the nested array it is in) and the second index identifies the “column” (the position of the element within the nested array). In the example above, the number represents the row and the letter represents the column.</p>
-<p>We can index into a grid with the pattern <code class="inline">array[row][column]</code>.</p>
-<p>For example, in the <code class="inline">tic_tac_toe</code> array above, we can access the center tic tac toe box by writing <code class="inline">tic_tac_toe[1][1]</code>, since it is in the 1st row, 1st column. (Remember, arrays start counting at zero!)</p>
+<div><p>In this challenge, we will start creating some data structures and have a better understanding on what’s going on under the hood with arrays and hashes. A powerful way to create an array is using the <code class="inline">Array.new</code> class method.</p>
+<pre><code class="ruby hljs">first_25_letters = (<span class="hljs-string">'A'</span>..<span class="hljs-string">'Z'</span>).first(<span class="hljs-number">25</span>)
+
+letter_grid = Array.new(<span class="hljs-number">5</span>) { first_25_letters.shift(<span class="hljs-number">5</span>) }
+<span class="hljs-comment"># =&gt; [["A", "B", "C", "D", "E"],</span>
+<span class="hljs-comment"># ["F", "G", "H", "I", "J"],</span>
+<span class="hljs-comment"># ["K", "L", "M", "N", "O"],</span>
+<span class="hljs-comment"># ["P", "Q", "R", "S", "T"],</span>
+<span class="hljs-comment"># ["U", "V", "W", "X", "Y"] ]</span></code></pre>
+<p>Do you understand how the above code is working? If not, make a note to come back later so you understand passing blocks and the <code class="inline">shift</code> method.</p>
 <h3>Tasks</h3>
-<h4>1. Chessboard</h4>
-<p>Build an array to represent a chess board. Populate it with the appropriate pieces represented as strings, i.e. “W Knight”, “B Pawn”, “B Queen”, etc. Don’t overthink this! It’s as simple as it seems. This may help:</p>
-<p><img src="http://www.freemasons-freemasonry.com/game_chess08.jpg" alt="Orang putih chess"></p>
-<p>There are many ways to approach this problem. You can hardcode, or you can make use of loops to help you fill the chessboad.&nbsp;If you cringe at the thought of creating the whole board by hand and how much repetition that will be, create a method that generates the board, and DRY it up! We will leave it up to you to decide which works better. But try not to spend too much time on this! You can always come back later to improve your code.</p>
-<pre><code class="ruby hljs"><span class="hljs-comment"># Example usage:</span>
-puts chessboard[<span class="hljs-number">7</span>][<span class="hljs-number">0</span>] == <span class="hljs-string">"W Rook"</span> <span class="hljs-comment"># outputs true</span></code></pre>
-<h4>2. Data table</h4>
-<p>Nested arrays can also be used as a way to store data in a table format, similar to a spreadsheet.</p>
-<p>You can use the first nested array to store the headers, and then each of the subsequent nested arrays can store the values in the particular column.</p>
-<p>Build an array to store this tabular data:</p>
-<pre><code class="ruby hljs"><span class="hljs-params">| Number |</span> Name <span class="hljs-params">| Position |</span> Points per Game <span class="hljs-params">|
-+---------+-------------+----------------+----------------------+
-  |</span> <span class="hljs-number">12</span> <span class="hljs-params">| Joe Schmo |</span> Center <span class="hljs-params">| [14, 32, 7, 0, 23] |</span>
-<span class="hljs-params">| 9 |</span> Ms. Buckets <span class="hljs-params">| Point Guard |</span> [<span class="hljs-number">19</span>, <span class="hljs-number">0</span>, <span class="hljs-number">11</span>, <span class="hljs-number">22</span>, <span class="hljs-number">0</span>] <span class="hljs-params">|
-|</span> <span class="hljs-number">31</span> <span class="hljs-params">| Harvey Kay |</span> Shooting Guard <span class="hljs-params">| [0, 30, 16, 0, 25] |</span>
-<span class="hljs-params">| 18 |</span> Sally Talls <span class="hljs-params">| Power Forward |</span> [<span class="hljs-number">18</span>, <span class="hljs-number">29</span>, <span class="hljs-number">26</span>, <span class="hljs-number">31</span>, <span class="hljs-number">19</span>] <span class="hljs-params">|
-|</span> <span class="hljs-number">22</span> <span class="hljs-params">| MK DiBoux |</span> Small Forward <span class="hljs-params">| [11, 0, 23, 17, 0] |</span></code></pre>
-<p>When completed, you should be able to index into <code class="inline">table</code> like this:</p>
-<pre><code class="ruby hljs">puts table[<span class="hljs-number">3</span>][<span class="hljs-number">2</span>] == <span class="hljs-string">"Shooting Guard"</span> <span class="hljs-comment"># outputs true</span>
-puts table[<span class="hljs-number">1</span>][<span class="hljs-number">3</span>] == [<span class="hljs-number">14</span>, <span class="hljs-number">32</span>, <span class="hljs-number">7</span>, <span class="hljs-number">0</span>, <span class="hljs-number">23</span>] <span class="hljs-comment"># outputs true</span></code></pre>
-</div>
+<h4>1. Tic-Tac-Toe Revisited - Populating a Board</h4>
+<p>Take a look at the simple 2 dimensional array for Tic-Tac-Toe from <a href="https://code.nextacademy.com/courses/web-development-bootcamp/challenges/data-drill-nested-arrays">the previous challenge</a>. Let’s say we wanted to generate 10 sample boards with x’s and o’s. How would you do that? How do you make the x’s and o’s random? In the first iteration, it’s OK to create boards that don’t have logical ratio of x’s and o’s.</p>
+    <p>Write some driver code with a test to make sure your method is working, and your resultant array has 9 elements. What <code class="inline">Array</code> method would you use?</p>
+    <p><strong>Extra Credit:</strong> Go ahead and populate a board with a realistic ratio of x’s and o’s. There should only be either 5 x’s or 4 o’s or vice versa. There’s an enumerable method that works on arrays that can help you! How could you write a good driver code test to make sure the array has the right number of x’s and o’s?</p>
+    <h4>2 Convert nested array to hash</h4>
+    <p>Ruby provides a handy <code class="inline">Hash::[]</code> method which will convert a properly formatted array into a hash. <a href="http://www.ruby-doc.org/core-1.9.3/Hash.html#method-c-5B-5D">Read the documentation</a>.</p>
+    <p>Convert each entry (player) from the data table array from the previous challenge (shown below) into a hash. You will need to write code that modifies the original array into the right format that the <code class="inline">Hash::[]</code> method expects to get the proper result.</p>
+    <pre><code class="ruby hljs">roster = [[<span class="hljs-string">"Number"</span>, <span class="hljs-string">"Name"</span>, <span class="hljs-string">"Position"</span>, <span class="hljs-string">"Points per Game"</span>],
+                                           [<span class="hljs-number">12</span>, <span class="hljs-string">"Joe Schmo"</span>, <span class="hljs-string">"Center"</span>, [<span class="hljs-number">14</span>, <span class="hljs-number">32</span>, <span class="hljs-number">7</span>, <span class="hljs-number">0</span>, <span class="hljs-number">23</span>] ],
+                                           [<span class="hljs-number">9</span>, <span class="hljs-string">"Ms. Buckets"</span>, <span class="hljs-string">"Point Guard"</span>, [<span class="hljs-number">19</span>, <span class="hljs-number">0</span>, <span class="hljs-number">11</span>, <span class="hljs-number">22</span>, <span class="hljs-number">0</span>] ],
+                                           [<span class="hljs-number">31</span>, <span class="hljs-string">"Harvey Kay"</span>, <span class="hljs-string">"Shooting Guard"</span>, [<span class="hljs-number">0</span>, <span class="hljs-number">30</span>, <span class="hljs-number">16</span>, <span class="hljs-number">0</span>, <span class="hljs-number">25</span>] ],
+                                           [<span class="hljs-number">7</span>, <span class="hljs-string">"Sally Talls"</span>, <span class="hljs-string">"Power Forward"</span>, [<span class="hljs-number">18</span>, <span class="hljs-number">29</span>, <span class="hljs-number">26</span>, <span class="hljs-number">31</span>, <span class="hljs-number">19</span>] ],
+                                           [<span class="hljs-number">22</span>, <span class="hljs-string">"MK DiBoux"</span>, <span class="hljs-string">"Small Forward"</span>, [<span class="hljs-number">11</span>, <span class="hljs-number">0</span>, <span class="hljs-number">23</span>, <span class="hljs-number">17</span>, <span class="hljs-number">0</span>] ]]</code></pre>
+    <h4>3. Break It Down</h4>
+    <p>Take a moment to read up in the <a href="http://www.ruby-doc.org/core-1.9.3/Hash.html#method-c-5B-5D">documentation</a>&nbsp;to see the 3 formats that the <code class="inline">Hash::[]</code> method accepts.</p>
+    <p>Which format seems most convenient, given the nested array shown above? Test out the <code class="inline">Hash::[]</code> method with some test data in <code class="inline">irb</code>. How does your nested array need to change so it is the right format?</p>
+    <p>Create a method that can converts the nested array appropriately:</p>
+    <pre><code class="ruby hljs"><span class="hljs-function"><span class="hljs-keyword">def</span> <span class="hljs-title">convert_roster_format</span><span class="hljs-params">(roster)</span></span>
+    <span class="hljs-comment"># your code here</span>
+    <span class="hljs-comment"># should return a new format of roster which is an array of hashes</span>
+    <span class="hljs-keyword">end</span>
+
+    hashed_roster = convert_roster_format(roster)
+
+    <span class="hljs-comment"># Now, you should have an array of hash objects, which will allow you to write driver code like this:</span>
+
+    hashed_roster[<span class="hljs-number">2</span>]
+    <span class="hljs-comment"># =&gt; { "Number" =&gt; 31, "Name" =&gt; "Harvey Kay", "Position" =&gt; "Shooting Guard", "Points per Game" =&gt; [0, 30, 16, 0, 25] }</span>
+
+    puts hashed_roster[<span class="hljs-number">0</span>][<span class="hljs-string">"Name"</span>] == <span class="hljs-string">"Joe Schmo"</span> <span class="hljs-comment"># outputs true</span></code></pre>
+    <p>Write some more driver code to test your new array of hashes!</p>
+    </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,68 +1,51 @@
 require 'rspec'
 require_relative 'nested_arrays'
 describe "nested_arrays.rb" do
-  describe "chessboard" do
-    it "contains pieces using the correct naming convention(e.g. \"W Pawn\")" do
-      chess_regex = /[W|B][ ](Pawn|King|Queen|Rook|Knight|Bishop)/
-      expect(chessboard[rand(0..1)][rand(0..1)]).to match chess_regex
-      expect(chessboard[rand(6..7)][rand(6..7)]).to match chess_regex
-    end
-
-    it "is a nested array containing rows of chess pieces" do
-      expect(chessboard.length).to eq 8
-      expect(chessboard[rand(0..7)].length).to eq 8
-    end
-
-    it "contains correct chess pieces at correct columsn" do
-      expect(chessboard[0][0]).to match /[W|B][ ]Rook/
-      expect(chessboard[0][1]).to match /[W|B][ ]Knight/
-      expect(chessboard[0][2]).to match /[W|B][ ]Bishop/
-      expect(chessboard[0][3]).to match /[W|B][ ]Queen/
-      expect(chessboard[0][4]).to match /[W|B][ ]King/
-      expect(chessboard[0][5]).to match /[W|B][ ]Bishop/
-      expect(chessboard[0][6]).to match /[W|B][ ]Knight/
-      expect(chessboard[0][7]).to match /[W|B][ ]Rook/
-
-      expect(chessboard[1][0]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][1]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][2]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][3]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][4]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][5]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][6]).to match /[W|B][ ]Pawn/
-      expect(chessboard[1][7]).to match /[W|B][ ]Pawn/
-
-      expect(chessboard[7][0]).to match /[W|B][ ]Rook/
-      expect(chessboard[7][1]).to match /[W|B][ ]Knight/
-      expect(chessboard[7][2]).to match /[W|B][ ]Bishop/
-      expect(chessboard[7][3]).to match /[W|B][ ]Queen/
-      expect(chessboard[7][4]).to match /[W|B][ ]King/
-      expect(chessboard[7][5]).to match /[W|B][ ]Bishop/
-      expect(chessboard[7][6]).to match /[W|B][ ]Knight/
-      expect(chessboard[7][7]).to match /[W|B][ ]Rook/
-
-      expect(chessboard[6][0]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][1]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][2]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][3]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][4]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][5]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][6]).to match /[W|B][ ]Pawn/
-      expect(chessboard[6][7]).to match /[W|B][ ]Pawn/
+  describe "#generate_realistic_tic_tac_toe" do
+    it "has the right ratio of 'x' and 'o' (both lowercase letters)" do
+      result = generate_realistic_tic_tac_toe.flatten.map(&:downcase)
+      expect(result.length).to eq(9)
+      expect(result.count('x')).to be >=(4)
+      expect(result.count('o')).to be >=(4)
     end
   end
 
-  describe "#table" do
-    it "should contain data in correct format" do
-      target = [
+  describe "#convert_roster_format" do
+    it "converts the nested array into an array of hashes" do
+      roster =
+      [
         ["Number", "Name", "Position", "Points per Game"],
         [12, "Joe Schmo", "Center", [14, 32, 7, 0, 23]],
         [9, "Ms. Buckets", "Point Guard", [19, 0, 11, 22, 0]],
         [31, "Harvey Kay", "Shooting Guard", [0, 30, 16, 0, 25]],
-        [18, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
+        [7, "Sally Talls", "Power Forward", [18, 29, 26, 31, 19]],
         [22, "MK DiBoux", "Small Forward", [11, 0, 23, 17, 0]]
       ]
-      expect(table).to eq target
+
+      convert_roster_format(roster)[0]["Number"] = 12
+      convert_roster_format(roster)[0]["Name"] = "Joe Schmo"
+      convert_roster_format(roster)[0]["Position"] = "Center"
+      convert_roster_format(roster)[0]["Points per Game"] = [14, 32, 7, 0, 23]
+
+      convert_roster_format(roster)[1]["Number"] = 9
+      convert_roster_format(roster)[1]["Name"] = "Ms. Buckets"
+      convert_roster_format(roster)[1]["Position"] = "Point Guard"
+      convert_roster_format(roster)[1]["Points per Game"] = [19, 0, 11, 22, 0]
+
+      convert_roster_format(roster)[2]["Number"] = 31
+      convert_roster_format(roster)[2]["Name"] = "Harvey Kay"
+      convert_roster_format(roster)[2]["Position"] = "Shooting Guard"
+      convert_roster_format(roster)[2]["Points per Game"] = [0, 30, 16, 0, 25]
+
+      convert_roster_format(roster)[3]["Number"] = 7
+      convert_roster_format(roster)[3]["Name"] = "Sally Talls"
+      convert_roster_format(roster)[3]["Position"] = "Power Forward",
+        convert_roster_format(roster)[3]["Points per Game"] = [18, 29, 26, 31, 19]
+
+      convert_roster_format(roster)[4]["Number"] = 22
+      convert_roster_format(roster)[4]["Name"] = "MK DiBoux"
+      convert_roster_format(roster)[4]["Position"] = "Small Forward"
+      convert_roster_format(roster)[4]["Points per Game"] = [11, 0, 23, 17, 0]
     end
   end
 end
