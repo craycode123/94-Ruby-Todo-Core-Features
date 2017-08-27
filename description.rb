650d0d00e6556eb2a1ac0654a0aa409752b39de3
jtcy0123 <jasmin0123@hotmail.com>
2017-08-27 13:44:30 +0800

    first commit

--- description.rb
+++ description.rb
@@ -1,7 +1,7 @@
-<div><p>Starting with your attempt from the previous iteration of the Sudoku solver, we now want to add the ability to make guessing.</p>
-<p>What happens if a square/cell can contain multiple possible numbers and you don’t have enough information to conclude right then and there which number it is?</p>
-<p>Most people who play Sudoku do “guessing”. Usually this is done by writing possibilities in the corners of the square.</p>
-  <h3>Task: Implement a guessing-friendly version of Sudoku</h3>
-  <p>Once we’ve filled in all cells which have only one possible value, we have to guess. Write out a pseudocode for how that might work before you dive in!</p>
-  <p>This solver should now be able to solve any Sudoku puzzle, although some will take a long time. Try it with the <code class="inline">sample_unsolved.txt</code> file.</p>
+<div><p>Ruby provides a handy shortcut for creating <em>accessor methods</em>. Read <a href="http://stackoverflow.com/questions/5046831/why-use-rubys-attr-accessor-attr-reader-and-attr-writer">this Stack Overflow answer</a>&nbsp;and <a href="http://stackoverflow.com/questions/4370960/what-is-attr-accessor-in-ruby">the comments here</a>&nbsp;on how they work.</p>
+<h3>Objectives</h3>
+<h4>Replace with attr_helpers</h4>
+<p>Read over the Ruby program provided until you understand the basic structure. Then load it into irb or <a href="http://pryrepl.org/">pry</a>&nbsp;and play around with it.</p>
+<p>Next, identify which methods could be replaced with an <code class="inline">attr_reader</code>, <code class="inline">attr_writer</code>, or <code class="inline">attr_accessor</code> helper and do it.</p>
+  <p>Write driver code to ensure your code still works in the same way. The purpose of this challenge is to refactor for clarity and concision, not alter the behavior of the program.</p>
   </div>
--- rspec_test.rb
+++ rspec_test.rb
@@ -1,19 +1,46 @@
 require 'rspec'
-require_relative 'sudoku'
-describe "Sudoku" do
-  let(:board_string) { "800000500004000000005800906003070200540000000700054000000200001300000000008900000" }
-  let(:new_sudoku_object) { Sudoku.new(board_string) }
-  let(:solution) { "839716524674592138215843976963178245541329867782654319497235681356481792128967453" }
+require_relative 'company'
 
-  describe "initializing a new Sudoku object" do
-    it "should only take in a string argument" do
-      expect(new_sudoku_object).to be_a(Sudoku)
-    end
+def should_have_attr_accessor(obj, column_name)
+  file = File.read "company.rb"
+  expect(file).to match(/attr_accessor.+#{column_name}/), "expected attr_accessor for #{column_name}, but got none"
+
+  writer = column_name.to_s + "="
+  expect(obj).to respond_to column_name
+  expect(obj).to respond_to writer.to_sym
+end
+
+def should_have_attr_reader(obj, column_name)
+  file = File.read "company.rb"
+  expect(file).to match(/attr_reader.+#{column_name}/), "expected attr_reader for #{column_name}, but got none"
+
+  expect(obj).to respond_to column_name
+end
+
+describe "Company" do
+  let(:company) { Company.new('Next Academy') }
+
+  it "should have attr_accessor :company_name" do
+    should_have_attr_accessor(company, :company_name)
   end
 
-  describe "#solve!" do
-    it "should be able to solve any string" do
-      expect(new_sudoku_object.solve!).to eq(solution)
-    end
+  it "should have attr_reader :employees" do
+    should_have_attr_reader(company, :employees)
+  end
+end
+
+describe "Employee" do
+  let(:employee) { Employee.new("Josh", "Master Of The Bootiverse") }
+
+  it "should have attr_accessor :name" do
+    should_have_attr_accessor(employee, :name)
+  end
+
+  it "should have attr_accessor :position" do
+    should_have_attr_accessor(employee, :position)
+  end
+
+  it "should have attr_reader :id" do
+    should_have_attr_reader(employee, :id)
   end
 end
